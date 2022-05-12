756f567f283f9e363f20e29dfe1aba295a3bfab1
Omer Gilboa <87078628+omergilboa@users.noreply.github.com>
2022-05-12 13:37:47 +0300

    test

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 const express = require('express');
 const api = require('./api');
 const API_ROUTES = require('./constants/routes');
+const http = require('./services/http');
+const twitterConfig = require('./external/twitter/config');
 
 dotenv.config();
 
@@ -10,6 +12,20 @@ const server = express();
 server.get('/', (req, res) => {
   res.send('GO TO /api TO GET API ENDPOINTS!');
 });
+
+(async () => {
+  try {
+    const userId = '1137030531210653696';
+    const url = `https://api.twitter.com/2/users/${userId}`;
+    const headers = twitterConfig.authorization();
+    const res = await http.get(url, {
+      'user.fields': 'withheld',
+    }, headers);
+    console.log('RESULT:', res.data.data);
+  } catch (err) {
+    console.error('ERROR:', err);
+  }
+})();
 
 server.use(API_ROUTES.ROOT, api);
 
